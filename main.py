ada13cf59d1a5c069a36d0f58542939bf0923df9
RaihanKabir277 <raihank0198@gmail.com>
2025-08-16 09:40:23 +0600

    window and background color added here

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Projects starts here .......")
+# print("Projects starts here .......")
+
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Fleshy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
+
+window.mainloop()
